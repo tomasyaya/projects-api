6cd04289d2ab342cb748d0b8a2707560daf5b171
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2022-02-05 14:30:45 +0100

    add ref to user

--- modules/project/project.model.js
+++ modules/project/project.model.js
@@ -4,7 +4,7 @@ const projectSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
   tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
-  owner: { type: mongoose.Schema.Types.ObjectId },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
 module.exports = mongoose.model("Project", projectSchema);
